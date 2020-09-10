18932777e5af19f994591b4180a9ba4c95375f74
Ahmad Tantowi <ahmadtantowi@outlook.com>
2020-09-10 14:30:55 +0700

    Add extension to set logger factory from app host

---
+++ src/FetchData/Extensions/FetchDataLoggerFactoryExtension.cs
@@ -0,0 +1,16 @@
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace FetchData.Extensions
+{
+    public static class FetchDataServiceProviderExtension
+    {
+        public static ILoggerFactory SetFetchDataLoggerFactory(this ILoggerFactory loggerFactory)
+        {
+            LogProvider.SetLogFactory(loggerFactory);
+
+            return loggerFactory;
+        }
+    }
+}
